bbbd60d2dbd509529c5747336b1a2621b1764b48
unknown <peluffojuancruz@gmail.com>
2022-09-28 13:07:19 -0300

    REGISTER (API)

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
+import protect from '../middlewares/auth.js';
 import User from '../models/User.js';
 import generateToken from '../utils/generateToken.js';
 
@@ -20,6 +21,47 @@ router.route('/login').post(
     } else {
       res.status(401);
       throw new Error('Invalid Email or Password');
+    }
+  })
+);
+
+router.route('/').post(
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+    const user = await User.create({ name, email, password });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid User data');
+    }
+  })
+);
+
+router.route('/profile').get(
+  [protect],
+  asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt,
+      });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
     }
   })
 );
